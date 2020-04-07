5ac0e2a3fc57a2b57ea2249fd1eca3b0ebcc1fc5
Aleksey <Alekseevskiyal@gmail.com>
2020-04-07 15:14:44 +0300

    homework1

---
+++ hw1/script.js
@@ -0,0 +1,19 @@
+console.log("sfsaf");
+
+console.log("This is the outer level");
+console.group();
+console.log("Level 2");
+console.group();
+console.log("Back to level 2");
+console.groupEnd();
+console.log("Back to the outer level");
+
+console.error("just error");
+
+console.warn("attention!");
+
+console.table(["aaaaa", "sssss", "ddddd"]);
+
+console.time("answer time");
+alert("for time");
+console.timeEnd("answer time");
