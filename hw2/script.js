12268c1c28c882bff3016820040c78774fee20db
Aleksey <Alekseevskiyal@gmail.com>
2020-04-07 15:27:40 +0300

    home work2

---
+++ hw2/script.js
@@ -0,0 +1,8 @@
+let a = +prompt ('Год рождения?');
+let b = prompt ('Как тебя зовут?');
+alert (b + (2020 - a));
+
+let first = +prompt ("first");
+let second = +prompt ("second");
+let third = +prompt ("third");
+alert ((first + second + third)/3);  
